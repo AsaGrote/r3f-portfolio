99ba11eb6c808db2bd7054653cd47b98f8dba2ad
AsaGrote <agrote@calpoly.edu>
2023-10-17 23:27:16 -0700

    Project Setup

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
+  <body>
+    <h1>Asa Grote</h1>
+    <h2>Work In Progress</h2>
+  </body>
+)
+
+
+
